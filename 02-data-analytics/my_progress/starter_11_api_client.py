13b95f67439fd05cce15d63ae02d8ce1b6db5e77
Yuvaraj K <yuvaraj21.spinix@gmail.com>
2026-08-25 09:01:22 +0000

    API Client

--- 02-data-analytics/my_progress/starter_11_api_client.py
+++ 02-data-analytics/my_progress/starter_11_api_client.py
@@ -15,7 +15,7 @@ print(f"With API Key:\t\tHTTP {response_with_api_key.status_code}\t|\t{response_
 
 # 2. fetch_all_trades(page_size): loop fetch_page from page 1, accumulate data,
 #    stop once page >= total_pages.
-PAGE_SIZE = 3
+PAGE_SIZE = 5
 
 def fetch_all_trades(page_size = PAGE_SIZE):
     records = []
@@ -23,9 +23,11 @@ def fetch_all_trades(page_size = PAGE_SIZE):
     while True:
         response = requests.get(BASE_URL, headers=HEADERS, params = {"page": page, "page_size": PAGE_SIZE})
         if response.status_code == 429:
-            pass
+            retry_after = int(response.headers.get('Retry After', 1))
+            print(f"\tRate limit reached, please wait {retry_after}s before retrying page {page}")
+            time.sleep(retry_after)
+            continue
         body = response.json()
-        print(body)
         records.extend(body['data'])
         print(f"Page {page} successfully fetched")
         if page >= body['total_pages']:
@@ -38,11 +40,24 @@ print("\nFetching All Trades, Page by Page...")
 records = fetch_all_trades()
 df = pd.DataFrame(records)
 print(f"\nSuccessfully loaded {len(df)} records into a dataframe")
-print("Shape of Dataset: ", df.shape)
-print("First 5 Rows")
+print("\nShape of Dataset: ", df.shape)
+print("\nFirst 5 Rows")
 print(df.head())
 
 # 4. In fetch_all_trades, on a 429: read Retry-After, time.sleep(), retry the SAME page.
-   
+print("\nManually hitting the rate limit by calling the API multiple times")
+for i in range(11):
+    response = requests.get(BASE_URL, headers=HEADERS, params = {"page": 1, "page_size": 1})
+    print(f"\trequest {i+1}:\t HTTP {response.status_code}")
+    if response.status_code == 429:
+        print(f"\tRate limit reached, please wait {int(response.headers.get('Retry After', 1))}s before retrying page 1")
+        break
 
 # 5. Comment: would you recommend an API over a nightly batch extract for this dataset? Why?
+"""
+It depends on when and where the data is used. If the data is used on an application 
+and we need to fetch it for multiple users concurrently, rate limiting the API is a necessity.
+However, if large amounts of trade data is sent over to a data analytics warehouse for later processing,
+concurrency isn't a priority and the nightly batch extract would work.
+So it depends not just on the dataset but also on the situation.
+"""
