da56de9a3e16f944f5577e307d4f83bb0fd1800a
Yuvaraj K <yuvaraj21.spinix@gmail.com>
2026-09-22 04:42:05 +0000

    Movie Data - Repo Structure

--- 06-auth-service/00-moviehub/app.js
+++ 06-auth-service/00-moviehub/app.js
@@ -1,29 +1,6 @@
-const movies = [
-    {
-        id: 1,
-        title: "Interstellar",
-        genre: "Sci-Fi",
-        rating: 9
-    },
-    {
-        id: 2,
-        title: "Inception",
-        genre: "Sci-Fi",
-        rating: 8.8
-    },
-    {
-        id: 3,
-        title: "Titanic",
-        genre: "Romance",
-        rating: 8
-    }
-];
-
-console.log("All Movies");
-console.log(movies);
-
+import { movies } from "./movie-data.js";
 const popularMovies = movies.filter(
     movie => movie.rating >= 8.5
 );
-console.log ("\nPopular Movies");
+
 console.log(popularMovies);
---
+++ 06-auth-service/00-moviehub/movie-data.js
@@ -0,0 +1,20 @@
+export const movies = [
+    {
+        id: 1,
+        title: "Interstellar",
+        genre: "Sci-Fi",
+        rating: 9
+    },
+    {
+        id: 2,
+        title: "Inception",
+        genre: "Sci-Fi",
+        rating: 8.8
+    },
+    {
+        id: 3,
+        title: "Titanic",
+        genre: "Romance",
+        rating: 8
+    }
+];
