fb0ff6eb1a35060093cd786b7aee29dd845903f3
Yuvaraj K <yuvaraj21.spinix@gmail.com>
2026-09-22 05:59:29 +0000

    Create movies.data.ts

---
+++ 06-auth-service/00-moviehub/ts/src/movies/movies.data.ts
@@ -0,0 +1,18 @@
+interface Movie {
+    id: number;
+    title: string;
+    genre: string;
+    rating: number;
+    description?: string;
+}
+
+function getMovieTitle(movie: Movie): string {
+    return movie.title;
+}
+
+const movie: Movie = {
+    id: 1,
+    title: "Interstellar",
+    genre: "Sci-Fi",
+    rating: 9
+};
